aa9b57218b884f1c6161ba9d4092f9adb0c75705
prasadacharya <prasada@mail.usf.edu>
2020-03-03 16:35:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace NumberOfDuplicatesInArray
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+
+            int result = CountNonUnique(a);
+            Console.WriteLine(result);
+        }
+        static int CountNonUnique(int[] numbers)
+        {
+
+
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+
+            foreach (int i in numbers)
+
+                if (dict.ContainsKey(i))
+                {
+                    dict[i]++;
+
+
+
+                }
+                else
+                {
+                    dict.Add(i, 1);
+                }
+
+            int CountNonUnique = 0;
+            foreach (KeyValuePair<int, int> kvp in dict)
+            {
+                if (kvp.Value > 1)
+
+                    CountNonUnique++;
+
+            }
+
+            return CountNonUnique;
+        }
+
+
+    }
+}
